3b32ec042d4a135649bd525fef040166e2d89c9a
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-25 18:22:13 +0900

    First commit

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = "0.13.6"
+  backend "s3" {
+    bucket = "terraform-kaz-210925-tfstate"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
