5fd44a0ae2fd3fef0f98ae82d10197ccab1bdf56
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-25 20:29:22 +0900

    とりま、これでterraform initが成功。バージョンが上がったせいで構文変わってる？

--- backend.tf
+++ backend.tf
@@ -1,12 +1,19 @@
 terraform {
   required_version = "0.13.6"
-  backend "s3" {
+  /*  backend "s3" {
     bucket = "terraform-kaz-210925-tfstate"
     key    = "terraform.tfstate"
     region = "ap-northeast-1"
-  }
+  }*/
 }
 
 provider "aws" {
   region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-kaz-210925-tfstate"
+  versioning {
+    enabled = true
+  }
 }
