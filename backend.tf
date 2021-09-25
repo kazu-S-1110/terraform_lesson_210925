f318c2ef4cb3497dc111899c29c2d4440534a933
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-25 20:54:01 +0900

    terraform initがうまくいかない問題は認証情報を書いてなかったからでした。backend.tf修正。

--- backend.tf
+++ backend.tf
@@ -1,19 +1,13 @@
-terraform {
-  required_version = "0.13.6"
-  /*  backend "s3" {
-    bucket = "terraform-kaz-210925-tfstate"
-    key    = "terraform.tfstate"
-    region = "ap-northeast-1"
-  }*/
-}
-
 provider "aws" {
   region = "ap-northeast-1"
 }
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-kaz-210925-tfstate"
-  versioning {
-    enabled = true
+terraform {
+  required_version = "0.13.6"
+  backend "s3" {
+    bucket = "terraform-kaz-210925-tfstate"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
   }
 }
+
+
